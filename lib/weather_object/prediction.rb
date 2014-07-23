5c1b0ec529b6b3927dedf0305b3f9ccf2d3c82bc
Mark Gangl <mark@attackcorp.com>
2014-07-22 22:49:56 -0400

    add temperature, float and string values to prediction

--- lib/weather_object/prediction.rb
+++ lib/weather_object/prediction.rb
@@ -5,5 +5,9 @@ module WeatherObject
     include Virtus.model
 
     attribute :high, Attribute::Temperature
+    attribute :low, Attribute::Temperature
+    attribute :pop, Attribute::Float
+    attribute :icon, String
+    attribute :condition, String
   end
 end
--- spec/acceptance/forecast_spec.rb
+++ spec/acceptance/forecast_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe 'Forecast weather data' do
   it 'holds forecast weather data' do
     weather = WeatherObject.new
     weather.add_forecast(
-      high: 20
+      high: 20,
+      low: 5,
+      pop: '25%',
+      icon: 'sunny',
+      condition: 'partly cloudy'
     )
 
     expect(weather.forecast.first.high.to_s).to eq '20 C'
+    expect(weather.forecast.first.low.to_s).to eq '5 C'
+    expect(weather.forecast.first.pop.to_s).to eq '25.0'
+    expect(weather.forecast.first.icon.to_s).to eq 'sunny'
+    expect(weather.forecast.first.condition.to_s).to eq 'partly cloudy'
   end
 end
